57507ed673e41d92bdead5b8f9a8f17819e76057
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-01-12 00:15:15 +0100

    deps fixed

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use('/@huluvu424242',express.static(path.join(__dirname, 'node_modules/@huluvu424242/honey-speaker/dist')))
+  .use('/@huluvu424242',express.static(path.join(__dirname, 'node_modules/@huluvu424242/honey-webcomponents/dist')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
