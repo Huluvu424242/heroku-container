2dcbe6e31f3c4295a7f735272e4877e6c3239ec3
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-01-11 19:35:59 +0100

    remote heroku changed

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
+  .get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
