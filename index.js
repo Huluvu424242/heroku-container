60b058f79e550886b0c915cdf911c14ce11fc2f5
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-01-11 18:03:01 +0100

    delivery honey example

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use("/honey-suite",express.static(path.join(__dirname, 'node_modules/@huluvu424242/honey-speaker/dist')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
