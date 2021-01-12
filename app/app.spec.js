09788f1919eac4f8ed4bb811283fa68375535075
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-01-12 20:55:58 +0100

    test file moved

--- app/app.spec.js
+++ app/app.spec.js
@@ -1,12 +1,12 @@
 const { spawn } = require('child_process');
 const got = require('got');
-const test = require('tape');
+const appSpec = require('tape');
 
 // Start the app
 const env = Object.assign({}, process.env, {PORT: 5000});
 const child = spawn('node', ['index.js'], {env});
 
-test('responds to requests', (t) => {
+appSpec('responds to requests', (t) => {
   t.plan(4);
 
   // Wait until the server is ready
