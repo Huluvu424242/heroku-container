3df90420b2e45524daa776e7018a33aa5beb1f21
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-03-18 21:31:32 +0100

    [feature/#9-leveldb] leveldb anbindung
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const feeder = require('@huluvu424242/liona-feeds');
 
-express()
+const server = express()
     .use(express.static(path.join(__dirname, '../public')))
     .use('/@huluvu424242/honey-news', express.static(path.join(__dirname, '../node_modules/@huluvu424242/honey-news/dist/')))
     .use(feeder.addCORSHeader)
@@ -28,3 +28,37 @@ express()
     // })
 
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// Datenbank leveldb mit client replicas via websocket
+
+const level = require('level')
+const SubLevel = require('level-sublevel')
+const Replicate = require('level-replicate')
+
+// Datenbank initialisieren.
+const db = SubLevel(level('ring-db'));
+
+// Replication hinzufügen
+const master = Replicate(db, 'master', "MASTER-1");
+
+// Replication zwischen verbundenen Clients aufsetzen.
+
+const  WebSocket  = require('ws');
+
+const wss = new WebSocket.Server({ server });
+// wss.on('connection', (ws) => {
+//     console.log('Client connected');
+//     // ws.stream.pipe(master.createStream({tail: true})).pipe(stream);
+//     ws.on('close', () => console.log('Client disconnected'));
+// });
+
+const stream = WebSocket.createWebSocketStream(wss, { encoding: 'utf8' });
+
+stream.pipe(master.createStream({tail: true})).pipe(stream);
+// process.stdin.pipe(duplex);
+
+
+//
+// const wss = require('websocket-stream').createServer({server}, (stream) => {
+//     stream.pipe(master.createStream({tail: true})).pipe(stream);
+// })
