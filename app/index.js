f8519dfaf0e84903b888a9b23904d12072901e6b
Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>
2021-04-25 20:45:04 +0200

    [release/0.0.15] redirect rückbau
    Signed-off-by: Huluvu424242 alias Thomas Schubert <huluvu424242@gmail.com>

--- app/index.js
+++ app/index.js
@@ -7,11 +7,11 @@ express()
     .use(express.static(path.join(__dirname, '../public')))
     .use('/@huluvu424242/honey-news', express.static(path.join(__dirname, '../node_modules/@huluvu424242/honey-news/dist/')))
     .use(feeder.addCORSHeader)
-    .use(function(req, res, next) {
-       if( res.status(404)){
-           redirect404(computeSegmentCount(2, 0));
-       }
-    })
+    // .use(function(req, res, next) {
+    //    if( res.status(404)){
+    //        redirect404(computeSegmentCount(2, 0));
+    //    }
+    // })
     .get('/feed/', (req, res) => {
         const uuid = req.query.uuid;
         if (uuid) {
@@ -34,55 +34,3 @@ express()
 
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-
-
-////
-//// inhalt von redirect.js weil import unklar
-///
-
-// Single Page Apps for GitHub Pages
-// https://github.com/rafrex/spa-github-pages
-// Copyright (c) 2016 Rafael Pedicini, licensed under the MIT License
-// modified by Huluvu424242 at 2021
-
-const isLocal = () => {
-    const originURL = window.location.origin;
-    return originURL.startsWith("http://localhost") || originURL.startsWith("https://localhost")
-};
-
-const computeSegmentCount = (localSegmentCount,remoteSegmentCount) => {
-    return isLocal()? localSegmentCount:remoteSegmentCount;
-};
-
-const redirect404 = (segmentCount) => {
-    const location = window.location;
-    const origin = location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '');
-    const base = location.pathname.split('/').slice(0, 1 + segmentCount).join('/');
-
-    location.replace(
-        origin +
-        base + '/?p=/' +
-        location.pathname.slice(1).split('/').slice(segmentCount).join('/').replace(/&/g, '~and~') +
-        (location.search ? '&q=' + location.search.slice(1).replace(/&/g, '~and~') : '') +
-        location.hash
-    );
-};
-
-const recieveRedirect = () => {
-    (function (location) {
-        if (location.search) {
-            var q = {};
-            location.search.slice(1).split('&').forEach(function (v) {
-                var a = v.split('=');
-                q[a[0]] = a.slice(1).join('=').replace(/~and~/g, '&');
-            });
-            if (q.p !== undefined) {
-                window.history.replaceState(null, null,
-                    location.pathname.slice(0, -1) + (q.p || '') +
-                    (q.q ? ('?' + q.q) : '') +
-                    location.hash
-                );
-            }
-        }
-    }(window.location));
-};
